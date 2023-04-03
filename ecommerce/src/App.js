f57cc20856dba8dc3194b47b53e92f75bb06f6a0
masdefry <muhammaddefryan@gmail.com>
2023-04-03 10:39:36 +0700

    Layouting Register

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Components
+import Navbar from './Components/Navbar/Navbar';
+
+import { BsArrowRight } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className='flex px-24'>
+        <div className='flex-1'>
+          <div>
+            <h1 className='text-[52px] font-bold'>
+              REGISTRASI
+            </h1>
+          </div>
+          <div>
+            <h1 className='text-xl font-bold'> 
+              NAMA ANDA
+            </h1>
+            <input type='text' placeholder='Nama Depan' className='border border-gray-600 w-full mt-3 px-3 py-3 outline-none' />
+            <input type='text' placeholder='Nama Belakang' className='border border-gray-600 w-full mt-3 px-3 py-3 outline-none' />
+          </div>
+          <div className='pt-7'>
+            <h1 className='text-xl font-bold'> 
+              Tanggal Lahir
+            </h1>
+            <div className='flex gap-3'>
+              <input type='text' placeholder='Day' className='border border-gray-600 mt-3 px-3 py-3 outline-none' />
+              <input type='text' placeholder='Month' className='border border-gray-600 mt-3 px-3 py-3 outline-none' />
+              <input type='text' placeholder='Year' className='border border-gray-600 mt-3 px-3 py-3 outline-none' />
+            </div>
+          </div>
+          <div className='pt-7'>
+            <h1 className='text-xl font-bold'> 
+              Email
+            </h1>
+            <input type='text' placeholder='Email' className='border border-gray-600 w-full mt-3 px-3 py-3 outline-none' />
+          </div>
+          <div className='pt-7'>
+            <h1 className='text-xl font-bold'> 
+              Kata Sandi
+            </h1>
+            <input type='password' placeholder='Kata Sandi' className='border border-gray-600 w-full mt-3 px-3 py-3 outline-none' />
+          </div>
+          <div className='py-7'>
+            <div className='border border-gray-900 w-[130px] h-[50px] relative'>
+              <button className='flex items-center gap-3 bg-gray-900 text-white px-7 py-3 absolute top-[-3px]'>
+                KIRIM <BsArrowRight />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className='flex-1'>
+          Kanan
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ ecommerce/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+// React Icons
+import { AiOutlineSearch } from "react-icons/ai";
+import { SlBag } from "react-icons/sl";
+
+function Navbar() {
+  return (
+    // Section1: Navbar
+    <div className='px-10 border-b'>
+      <div className='flex justify-end gap-5 pt-1 text-[13px]'>
+        <div>
+          status pesanan
+        </div>
+        <div>
+          bantuan
+        </div>
+        <div>
+          newsletter
+        </div>
+        <div>
+          masuk
+        </div>
+      </div>
+      <div className='flex justify-between py-3'>
+        <div className='flex-1 relative top-[-20px]'>
+          Logo 
+        </div>
+        <div className='flex flex-1 justify-center items-center gap-6 text-[14px]'>
+          <div>
+            PRIA
+          </div>
+          <div>
+            WANITA
+          </div>
+          <div>
+            ANAK
+          </div>
+          <div>
+            OLAHRAGA
+          </div>
+        </div>
+        <div className='flex flex-1 justify-end items-center gap-3'>
+          <div>
+            <div className='flex items-center border border-gray bg-gray-100 px-3 py-1'>
+              <input type='text' className='bg-gray-100 outline-none' />
+              <AiOutlineSearch />
+            </div>
+          </div>
+          <div>
+            <SlBag className='text-lg' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
